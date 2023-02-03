d273bdc87af2e05f8f569cb3adbe7a80673beb56
Steadystudy <sgmin206@naver.com>
2023-02-03 23:15:33 +0900

    Solve: 큰 수 만들기

---
+++ problems/큰 수 만들기/index.js
@@ -0,0 +1,13 @@
+function solution(number, k) {
+  const stack = [];
+  for (let i = 0; i < number.length; i++) {
+    let el = number[i];
+    while (k > 0 && stack[stack.length - 1] < el) {
+      stack.pop();
+      k--;
+    }
+    stack.push(el);
+  }
+  stack.splice(stack.length - k, k);
+  return stack.join('');
+}
