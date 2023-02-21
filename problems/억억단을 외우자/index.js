618a2358db27077a9f389cd2e19bd0a1e27c32c1
Steadystudy <sgmin206@naver.com>
2023-02-21 20:13:00 +0900

    Solve: 억억단을 외우자

---
+++ problems/억억단을 외우자/index.js
@@ -0,0 +1,16 @@
+function solution(e, starts) {
+  const countArr = new Array(e + 1).fill(0);
+  for (let i = 1; i <= e; i++) {
+    for (let j = 1; j <= e / i; j++) {
+      countArr[i * j]++;
+    }
+  }
+  const sortedArr = countArr.map((c, i) => [c, i]).sort((a, b) => b[0] - a[0]);
+  return starts.map((start) => {
+    let i = 0;
+    while (start > sortedArr[i][1]) {
+      i++;
+    }
+    return sortedArr[i][1];
+  });
+}
